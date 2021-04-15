f0a77dc98b8f082c7dbc5806e06d8b9657f766b1
PopherTheG <pophertheg@gmail.com>
2021-04-15 20:30:07 +0800

    add ble function to send temperature data to phone

--- coffe-roaster-sketch/coffe-roaster-sketch.ino
+++ coffe-roaster-sketch/coffe-roaster-sketch.ino
@@ -9,12 +9,12 @@
 #include <SPI.h>
 
 #include "Adafruit_MAX31855.h"
-
 #include <BLEDevice.h>
-#include <BLEServer.h>
 #include <BLEUtils.h>
-#include <BLE2902.h>
+#include <BLEServer.h>
 
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 /* Defining pins to be usd for SPI communication */
 #define MAXDO 3
@@ -24,30 +24,35 @@
 /* Initialize the Thermocouple */
 Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
 
-/* */
-BLECharacteristic *characteristicTX;
-
+BLECharacteristic *pCharacteristic;
+char tempDataBuf[20];
 bool deviceConnected = false;
-
-#define SERVICE_UUID   "ab0828b1-198e-4351-b779-901fa0e0371e"
-#define CHARACTERISTIC_UUID_RX  "4ac8a682-9736-4e5d-932b-e9b31405049c"
-#define CHARACTERISTIC_UUID_TX  "0972EF8C-7613-4075-AD52-756F33D4DA91"
-
-class ServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-    }
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-    }
-};
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  while (!Serial) delay(1); // wait for Serial
 
   Serial.println("MAX31855 test");
+
+  BLEDevice::init("Long name works now");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setValue("Hello World says Neil");
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
 
   /* Wait for MAX chip to stabilize */
   delay(500);
@@ -56,68 +61,23 @@ void setup() {
     Serial.println("ERROR.");
     while (1) delay(10);
   }
-
-  /* Create the BLE Device */
-  BLEDevice::init("ESP32-BLE");
-
-  /* Create BLE Server */
-  BLEServer *server = BLEDevice::createServer();
-  server->setCallbacks(new ServerCallbacks());
-
-  /* Create the BLE Service */
-  BLEService *service = server->createService(SERVICE_UUID);
-
-  /* Create a BLE characteristic */
-  characteristicTX = service->createCharacteristic(CHARACTERISTIC_UUID_TX, BLECharacteristic::PROPERTY_NOTIFY);
-
-  characteristicTX->addDescriptor(new BLE2902());
-
-  /* Create a BLE characteristic */
-  BLECharacteristic *characteristic = service->createCharacteristic(CHARACTERISTIC_UUID_RX, BLECharacteristic::PROPERTY_WRITE);
-
-  characteristic->setCallbacks(new BLECharacteristicCallbacks());
-
-  /* Start the service */
-  service->start();
-
-  /* Start advertising */
-  server->getAdvertising()->start();
-
-  Serial.println("DONE");
 }
 
 void loop() {
-  float internal_temp = thermocouple.readInternal();
-  float thermocouple_temp = thermocouple.readCelsius();
-  Serial.print(internal_temp); Serial.print("  "); Serial.println(thermocouple_temp);
-  Serial.print(thermocouple.readInternal()); Serial.print("  "); Serial.println(thermocouple.readCelsius());
-  Serial.println(" ");
-  if (isnan(thermocouple_temp)) {
-    Serial.println("Something wrong with thermocouple");
-  }
-  if (deviceConnected)
-  {
-    char txString[8];
-    dtostrf(thermocouple_temp, 2, 2, txString);
+  /* basic readout test, just print the current temp */
+  Serial.print("Internal Temp = ");
+  Serial.println(thermocouple.readInternal());
 
-    characteristicTX->setValue(txString);
-    characteristicTX->notify();
-    Serial.println(txString);
+  double c = thermocouple.readCelsius();
+  if (isnan(c)) {
+    Serial.println("Something wrong with thermocouple!");
+  } else  {
+    Serial.print("C = ");
+    Serial.println(c);
   }
+  Serial.print("F = ");
+  Serial.println(thermocouple.readFahrenheit());
+  sprintf(tempDataBuf, "%lf %lf", thermocouple.readInternal(), thermocouple.readCelsius());
+  pCharacteristic->setValue(tempDataBuf);
   delay(1000);
 }
-
-/* basic readout test, just print the current temp */
-//  Serial.print("Internal Temp = ");
-//  Serial.println(thermocouple.readInternal());
-//
-//  double c = thermocouple.readCelsius();
-//  if (isnan(c)) {
-//    Serial.println("Something wrong with thermocouple!");
-//  } else  {
-//    Serial.print("C = ");
-//    Serial.println(c);
-//  }
-//  Serial.print("F = ");
-//  Serial.println(thermocouple.readFahrenheit());
-//  delay(1000);
